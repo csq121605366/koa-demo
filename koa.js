6e816b02f82d1d1dc8d31762baba6bccb2fc0456
caishangqing <121605366@qq.com>
2018-03-18 10:39:36 +0800

    435

---
+++ koa.js
@@ -0,0 +1,30 @@
+
+var koa = require('koa');
+const app = new koa();
+
+const asyncIO = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      Math.random() - 1 >= 0 ? resolve() : reject();
+    }, 1e3);
+  })
+}
+
+const mid = () => async (ctx, next) => {
+  ctx.body = 'mark';
+  await next();
+  ctx.body = ctx.body + ' done';
+}
+
+app.use(mid());
+
+app.use(async (ctx, next) => {
+  await asyncIO().then(() => {
+    ctx.body = ctx.body + ' saved';
+  }).catch(() => {
+    ctx.body = ctx.body + ' not saved';
+  })
+  await next();
+})
+
+app.listen(8080);
